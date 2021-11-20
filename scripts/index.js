67d906d3ed681e553bf39fcc90e223c0f709b51d
Alex Putilin <fobiya1982@yandex.ru>
2021-11-20 15:13:40 +0300

    v1.0

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const openPopup = document.querySelector('.profile__button-edit');
+const closePopup = document.querySelector('.popup__button-close');
+const editPopup = document.querySelector('.popup');
+
+function openPop() {
+  editPopup.classList.add('popup_opened');
+}
+openPopup.addEventListener('click', openPop);
+
+function closePop() {
+  editPopup.classList.remove('popup_opened');
+}
+closePopup.addEventListener('click', closePop);
+
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__name');
+let jobInput = formElement.querySelector('.form__job');
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  let profileName = nameInput.value;
+  let profileJob = jobInput.value;
+  console.log(profileName);
+  console.log(profileJob);
+  document.querySelector('.profile__title').textContent = profileName;
+  document.querySelector('.profile__job').textContent = profileJob;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
