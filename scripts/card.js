6e0be9796f14278224977d81f1d58c4fe40c1cc2
Alex Putilin <fobiya1982@yandex.ru>
2021-12-25 16:23:45 +0300

    Card-OOP-done

--- scripts/card.js
+++ scripts/card.js
@@ -1,32 +1,38 @@
-// import initialCards from "./cards";   //импортировали массив карточек
-
-class Card {                            // создали класс , должны передать ему селектор  и свойста карточек, IMAGE(card__image), TITLE(card__text)
+import openPop from "./index.js";
+class Card {
   constructor(card) {
-    console.log(card);          // так же по идее доджны добавить кнопки: Удалить и Сердечко
-    //this._selector = selector;
+
     this._image = card.link;
     this._title = card.name;
-                                  //присваиваем внутренним преемееным входные данные
-  }
-_getTemplate() {        //добавляем метод получния template элемента и кланирования его в DOM, на выходу return Template элемент
-  return document.querySelector('.template_card')
-                 .content
-                 .querySelector('.card')
-                .cloneNode(true)
-}
-;
-getCard() {
-  this._element = this._getTemplate();
-  console.log(this._element);
-  this._element.querySelector('.card__text').textContent = this._title;
-  this._element.querySelector('.card__image').src = this._image;
-  this._element.querySelector('.card__image').alt = this._title;
-  // this._element.querySelector('.todo-item__del').addEventListener('click', this._handleDeleteTodo);
 
-  return this._element;
-}
-// getView() {} добавилои метод полчения элемента путем присвавания свойст новому template элементу, на выходу return элемент со свойствами   34.01 webinarб
-                  //перебираем массив входных данных и вешаем на каждую новую карточку слушатеь на кнопку удалить
-// добавляем внутреннюю функцию удаления карточки
+  }
+  _getTemplate() {        //метод получения template элемента и кланирования его в DOM, на выходе  Template элемент
+    return document.querySelector('.template_card')
+      .content
+      .querySelector('.card')
+      .cloneNode(true)
+  };
+  getCard() {               //публичный метод присваивания вновь созданному элементу свойств и добавления слушателей на кнопки
+    this._tempCard = this._getTemplate();
+    this._tempCard.querySelector('.card__text').textContent = this._title;
+    this._tempCard.querySelector('.card__image').src = this._image;
+    this._tempCard.querySelector('.card__image').alt = this._title;
+    this._tempCard.querySelector('.card__button-delete').addEventListener('click', this._handleDeleteCard);
+    this._tempCard.querySelector('.card__button-heart').addEventListener('click', this._handleLikeCard);
+    this._tempCard.querySelector('.card__image').addEventListener('click', this._handlePopupOpen);
+    return this._tempCard;
+  }
+  _handlePopupOpen = () => {
+    document.querySelector('.popup__image').src = this._image;
+    document.querySelector('.popup__image').alt = this._title;
+    document.querySelector('.popup__image-caption').textContent = this._title;
+    openPop(document.querySelector('.popup_image'));
+  }
+  _handleDeleteCard = () => {   //метод удаления карточки
+    this._tempCard.remove();
+  }
+  _handleLikeCard = () => {     //метод срабатыаания лайка
+    this._tempCard.querySelector('.card__button-heart').classList.toggle('card__button-heart_active');
+  }
 }
 export default Card;
